81050ac3cdb252aa863587c228d9dea4b5b66c39
Siebe Albers <savdelz@gmail.com>
2023-07-06 16:26:34 +0200

    consolidating the two js scripts into one

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
+
+// part 1 - without this part pressing enter to submit the answer does NOT work.
+var submitClicked = false;
+var input = document.getElementById("answer-input");
+var submitButton = document.getElementById("submit-button");
+var nextButton = document.getElementById("next-button");
+var feedback = document.getElementById("feedback");
+
+// this function is called when the submit button is clicked
+input.addEventListener("keyup", function(event) {
+  event.preventDefault();
+  if (event.keyCode === 13) { // 13 is the enter key
+    if (!submitClicked) {
+      submitButton.click();
+    } else {
+      nextButton.click();
+    }
+  }
+});
+
+submitButton.addEventListener("click", function() { // when the submit button is clicked
+  submitClicked = true;
+  feedback.innerHTML = ""; // clear the feedback
+});
+
+nextButton.addEventListener("click", function() { // when the next button is clicked
+  submitClicked = false;
+  feedback.innerHTML = "";
+});
+
+
+// part 2
 // outline:
 // 1. fetch the data from the text file
 // 2. shuffle the array of Greek letters
